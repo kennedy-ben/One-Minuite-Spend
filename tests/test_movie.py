8e6125356392c7af9e88e1c11842ad097905b8e6
kennedy bernard <makuanikennedy@gmail.com>
2022-02-10 14:44:08 +0300

    added image icons

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -10,7 +10,7 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/Anything can work',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
